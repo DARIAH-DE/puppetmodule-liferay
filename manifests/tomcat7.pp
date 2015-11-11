44782c9b19a6883a8fbaa177555255ca4e7158c6
Ubbo Veentjer <veentjer@sub.uni-goettingen.de>
2015-11-11 13:27:43 +0100

    oracle-java for liferay

--- manifests/tomcat7.pp
+++ manifests/tomcat7.pp
@@ -23,6 +23,36 @@ class liferay::tomcat7 (
       'openjdk-6-jre-headless':   ensure => present;
     }
   }
+  if $java_version == 'oracle-jdk8' {
+
+    # copied from https://github.com/Spantree/puppet-java8/blob/master/manifests/init.pp
+    # ubuntu specific, for debian look at above link
+    # accept license
+    file { '/tmp/java.preseed':
+      source => 'puppet:///modules/liferay/oracle-java.preseed',
+      mode   => '0600',
+      backup => false,
+    }
+
+    apt::ppa { 'ppa:webupd8team/java': }
+
+    package { 
+      'oracle-java8-installer':
+        ensure       => present,
+        responsefile => '/tmp/java.preseed',
+        require      => [Apt::Ppa['ppa:webupd8team/java'],File['/tmp/java.preseed']],
+        notify       => Service['tomcat7'],
+      ;
+      'oracle-java8-set-default': ensure => present;
+      'openjdk-7-jdk':            ensure => absent;
+      'openjdk-7-jre':            ensure => absent;
+      'openjdk-7-jre-headless':   ensure => absent;
+      'openjdk-7-jre-lib':        ensure => absent;
+      'openjdk-6-jre-headless':   ensure => absent;
+    }  
+
+  }
+
   package { 'tomcat7':
     ensure => present
   }
